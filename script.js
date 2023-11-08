562bb2cd4a73b0a59fe9029c21312e6519176b96
VishalNadig <82140384+VishalNadig@users.noreply.github.com>
2023-11-08 10:34:59 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Robotics Engineer", "Electrical Engineer", "Software Engineer", "Robotics Engineer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+    
+})
